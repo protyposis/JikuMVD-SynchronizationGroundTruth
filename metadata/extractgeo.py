8067616a932fb4b5f73645d43edf59fdd8f0bde8
Mario Guggenberger <mg@protyposis.net>
2014-09-30 09:05:20 +0200

    video offset analysis

---
+++ metadata/extractgeo.py
@@ -0,0 +1,78 @@
+import os
+import xml.etree.ElementTree as ET
+import csv
+import json
+import re
+
+# Parses the Jiku XML metadata files and creates CSV and JSON files 
+# for all videos with geolocation and compass data
+
+def localizeDE(infile, outfile):
+	fin = open(infile)
+	fout = open(outfile, "w")
+	for line in fin:
+	    fout.write( line.replace(',', ';').replace('.',',').replace(',xml','.xml') )
+	fin.close()
+	fout.close()
+
+def jsonToJs(intfile, outfile, variableName):
+	f = open(intfile,'r')
+	newf = open(outfile,'w')
+	lines = f.readlines()
+	newf.write(variableName + ' = ')
+	for line in lines:
+		newf.write(line)
+	newf.write(';')
+	newf.close()
+	f.close()
+
+indir = './metadata/'
+T = []
+M = {}
+
+for root, dirs, filenames in os.walk(indir):
+	filenames.sort()
+	for filename in filenames:
+		if filename.endswith('.xml'):
+			f = root + filename
+			xml = ET.parse(f)
+			xmlLocation = xml.find('.//location');
+			# print(f)
+			if list(xmlLocation): # check if xmlLocation is a list of subproperties; if not, it is a 'location missing' placeholder text
+				lat = float(xmlLocation.find('latitude').text) # samplemeanlatitude
+				lon = float(xmlLocation.find('longitude').text) # samplemeanlongitude
+				acc = float(xmlLocation.find('accuracy').text)
+				location = (lat, lon)
+				direction = float(xml.find('.//orientation/direction').text)
+				if lat != 0:
+					T.append((filename,) + (location) + (direction,))
+					M[filename] = { 'location':{ 'lat':lat, 'lon':lon, 'acc':acc }, 'dir':direction }
+			# else:
+			# 	print(xmlLocation.text)
+
+# write the CSV data file
+with open("_locations.csv", "w",newline='') as outfile:
+	writer = csv.writer(outfile, dialect = 'excel')
+	writer.writerows(T)
+
+# convert the CSV to a German localized format for easier handling in German Excel
+localizeDE('_locations.csv', '_locations-de.csv')
+
+# group the JSON data by the events
+M2 = {}
+for entry in M:
+	event = re.search('([A-Z]+_[0-9]+)', entry).group(1)
+	# print(event)
+	if event not in M2:
+		M2[event] = {}
+	M2[event][entry.replace('.xml','')] = M[entry]
+
+# write the JSON data to a file
+with open("_locations.json", "w") as outfile:
+    # json.dump({'numbers':1, 'strings':'bla', 'x':3, 'y':4}, outfile, indent=4)
+    json.dump(M2, outfile, indent=4)
+
+# convert the JSON file to an HTML-includable JS file containing the JSON data 
+# due to browser security, JSON files cannot be loaded from a local path -> a 
+# local webserver would be needed which is too much of a hassle for this simple task
+jsonToJs("_locations.json", "_locations.js", 'locations')
---
+++ metadata/setup.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import subprocess
+
+def is_tool(name):
+	try:
+		devnull = open(os.devnull)
+		subprocess.Popen([name], stdout=devnull, stderr=devnull).communicate()
+	except OSError as e:
+		if e.errno == os.errno.ENOENT:
+			return False
+	return True
+
+extension = None
+
+errorDlScriptMissing = """
+Jiku download file missing, please:
+ - visit http://liubei.ddns.comp.nus.edu.sg/jiku/dataset/download/
+ - select the desired videos (preferably all)
+ - download the download script archive
+ - extract the archive to this script's directory
+ - restart this script
+"""
+
+errorWgetMissing = """
+wget is missing, but required for downloading
+please download wget
+ - for windows (wget AND dependencies): http://gnuwin32.sourceforge.net/packages/wget.htm
+ - for linux: through your distribution's package manager
+"""
+
+print("checking for supported OS...")
+if os.name is 'nt':
+	extension = '.bat'
+elif os.name is 'posix':
+	extension = '.sh'
+else:
+	print("unsupported OS: " + os.name)
+	sys.exit(1)
+
+print("checking for download script...")
+if not os.path.isfile('download'+extension):
+	print(errorDlScriptMissing)
+	sys.exit(2)
+
+print("checking for wget...")
+if not is_tool("wget"):
+	print(errorWgetMissing)
+	sys.exit(3)
+
+print("extracting metadata XML files and thumbnails from download script...")
+with open("download"+extension, "r") as dlin, \
+		open("download-xml"+extension, "w") as dlxmlout, \
+		open("download-jpg"+extension, "w") as dljpgout:
+	for line in dlin:
+		if line.strip().endswith(".xml"):
+			line = line.replace('-N', '-N -P ./metadata/')
+			dlxmlout.write(line)
+		elif line.strip().endswith(".jpg"):
+			line = line.replace('-N', '-N -P ./thumbnails/')
+			dljpgout.write(line)
+
+print("downloading XML metadata files to 'metadata' subdirectory...")
+subprocess.call("download-xml"+extension)
+
+print("downloading JPEG thumbnail files to 'thumbnails' subdirectory...")
+subprocess.call("download-jpg"+extension)
+
+print("downloading finished!")
+print("preparing geo data...")
+execfile("extractgeo.py")
+print("finished! You can now open the map.html in the 'map' subdirectory.")
